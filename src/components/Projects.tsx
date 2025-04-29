3f7550c22692be3379eb6624d391c42a2d12541b
Syed Mubashir Ali <81847925+syedmubashiralii@users.noreply.github.com>
2025-04-29 15:59:31 +0500

    add new app brightflixx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,18 +13,18 @@ const Projects = () => {
       githubLink: "https://github.com/syedmubashiralii/BuddyTracker"
     },
     {
-      title: "Task Management App",
-      description: "A collaborative task management application with real-time updates and team workspace.",
+      title: "Pacer",
+      description: "Pacer is your personal step counter and walking tracker. Using a built-in pedometer, it records every step, distance walked, and your walking path.",
       image: "https://images.unsplash.com/photo-1517292987719-0369a794ec0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
-      tags: ["Vue.js", "Firebase", "Tailwind CSS", "Vuex"],
-      liveLink: "#",
-      githubLink: "#"
+      tags: ["Flutter", "Dart", "Google Map", "Firebase"],
+      liveLink: "https://play.google.com/store/apps/details?id=charisma.motiondetectorpedometer.steptrackercounter&hl=en",
+      githubLink: "https://github.com/syedmubashiralii/Pacer-Android"
     },
     {
-      title: "Finance Dashboard",
-      description: "An interactive dashboard for tracking personal finances with data visualization and budgeting tools.",
-      image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-      tags: ["React", "D3.js", "Express", "PostgreSQL", "Material UI"],
+      title: "BrightFlixx",
+      description: "BrightFlixx is your personalized entertainment hub – stream your favorite movies and shows, just like Netflix.",
+      image: "images/brightflixx-1.jpg",
+      tags: ["Flutter", "Dart", "Google Map", "Firebase"],
       liveLink: "#",
       githubLink: "#"
     },
